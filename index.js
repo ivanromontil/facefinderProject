d51dda610f415d0f87e2a35404b41434026049f2
ivanromontil <u1979279@campus.udg.edu>
2024-05-06 16:41:18 +0200

    probemos2

--- index.js
+++ index.js
@@ -3,25 +3,19 @@ import 'regenerator-runtime/runtime';
 const { BlobServiceClient } = require("@azure/storage-blob");
 
 const blobSasUrl = "https://facefinderstorageproject.blob.core.windows.net/?sv=2022-11-02&ss=b&srt=co&sp=wciytf&se=2024-05-06T22:15:37Z&st=2024-05-06T14:15:37Z&spr=https&sig=byAFNRrisAnSqXwys%2BHwajkbIUgbbLfknGXZgyp9gpE%3D"
-const BlobServiceClient = new BlobServiceClient(blobSasUrl);
+const blobServiceClient = new BlobServiceClient(blobSasUrl);
 
 const containerName = 'facestorage';
-const containerClient = BlobServiceClient.getContainerClient(contaierName);
+const containerClient = blobServiceClient.getContainerClient(contaierName);
 const selectButton = document.getElementById("select-button")
 
 const fileInput = document.getElementById('imageInput');
 const file = fileInput.files[0];
-const blobName = file.name;
-const blobServiceUrl = `https://facefinderstorageproject.blob.core.windows.net`;
-
-const blobUrl = `${blobServiceUrl}/${containerName}/${blobName}`;
-
 try {
 
-    // aaa
     const promises = []
     for(const file of fileInput.files){
-        const blockBlobClient = containerClinet.getBlockBlobClient(file.name);
+        const blockBlobClient = containerClient.getBlockBlobClient(file.name);
         promises.push(blockBlobClient.uploadBrowserData(file));
     }
 
